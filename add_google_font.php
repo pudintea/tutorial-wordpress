e7f3058728f1f487d90a9bffc7971b4bb9e9c887
Pudin Saepudin Ilham <najzmitea@gmail.com>
2022-11-09 13:56:16 +0700

    Create add_google_font.php

---
+++ add_google_font.php
@@ -0,0 +1,8 @@
+<?php
+function add_google_fonts() {
+	wp_enqueue_style( ' add_google_fonts ', ' https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800', false );
+}
+add_action( 'wp_enqueue_scripts', 'add_google_fonts' );
+
+// functions.php
+
