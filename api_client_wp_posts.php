cf4c8333ec6d348afe3b4948cb19c1f62c04b09a
Pudintea <najzmitea@gmail.com>
2023-01-03 15:33:05 +0700

    Add files via upload

---
+++ api_client_wp_posts.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta content="width=device-width, initial-scale=1" name="viewport" />
+		<title>API Wordpress dengan PHP</title>
+	</head>
+	<body>
+		<div>
+			<?php
+			$url = 'https://www.al-azhar.or.id/wp-json/wp/v2/posts';
+			$contents = file_get_contents($url);
+			$contents = utf8_encode($contents);
+			$reults = json_decode($contents, true);
+			$no = 0;
+			foreach ($reults as $result){
+				$no++;
+				echo '<h1>'.$no.'. <a href="'.$result['link'].'" target="_blank">'.$result['title']['rendered'].'</a></h1>';
+				//echo '<content>'.$result['content']['rendered'].'</content>';
+				echo '<content>'.$result['excerpt']['rendered'].'</content>';
+			}
+			?>
+		</div>
+	</body>
+</html>
