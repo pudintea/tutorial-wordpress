573d586f6b50f287f22dc9f2dd0c19b075d0aef0
Pudintea <najzmitea@gmail.com>
2023-01-10 16:16:41 +0700

    Update pdn_head_schema.php

--- pdn-data/pdn_head_schema.php
+++ pdn-data/pdn_head_schema.php
@@ -25,6 +25,22 @@ if ( ! function_exists( 'pdn_header_schema_website' ) ) :
 		  }
 		}
 		</script>
+		<script type="application/ld+json">
+		{
+			"@context": "http://schema.org",
+			"@type": "BreadcrumbList",
+			"itemListElement": [
+				{
+					"@type": "ListItem",
+					"position": 1,
+					"item": {
+						"@id": "<?=get_home_url();?>",
+						"name": "Home"
+					}
+				}
+			]
+		}
+		</script>
 	<?php
 	  }
 	  
